48bc461686bebcaa12ca399afba16a6f344c0aa2
rennekampjeffrey <jeffrey.rennekamp@msj.edu>
2026-03-04 10:19:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// Array of menu items
 const menuItems = ["Latte", "Espresso", "Green Tea", "Muffin", "Iced Coffee"];
 
+// Select the unordered list
 const menuList = document.getElementById("menu-list");
 
+// Loop through array and create list items
 menuItems.forEach(function(item) {
     const li = document.createElement("li");
     li.textContent = item;
